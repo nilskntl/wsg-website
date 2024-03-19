1bf9fe6d0b0536ea4fd8b967fbb4d1f6da4b9519
Nils Knittel <go56bog@mytum.de>
2024-03-19 15:56:35 +0100

    fix: random slide

--- website/backend/random_slide/Start.js
+++ website/backend/random_slide/Start.js
@@ -1,22 +1,22 @@
-/**
- * Startseite
- */
 // Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 
 function startWithRandomSlide() {
-    /**
-     * Ändere die Slideshow auf ein zufälliges Bild
+    /*
+        * Ändere die Slideshow auf ein zufälliges Bild
      */
-    let rnd = Math.floor(Math.random() * 7);
-    $w('#slideshow1').changeSlide(rnd)
-        .then((newSlide) => {
-            $w('#slideshow1').show()
+    let rnd = Math.floor(Math.random() * 6);
+    $w('#slideshow1').hide() // Slideshow ausblenden
+        .then(() => {
+            return $w('#slideshow1').changeSlide(rnd); // Zufälliges Bild auswählen
+        })
+        .then(() => {
+            $w('#slideshow1').show(); // Slideshow wieder anzeigen
         });
 }
 
 $w.onReady(function () {
-    /**
-     * Führe beim Laden der Seite die folgenden Funktionen aus
+    /*
+        * Führe beim Laden der Seite die folgenden Funktionen aus
      */
     // Starte die Slideshow mit einem zufälligen Bild
     startWithRandomSlide();
